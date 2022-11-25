6bba2062499edf4ba21c1b8469ee531673898cd5
Diyamaurya467 <118798610+Diyamaurya467@users.noreply.github.com>
2022-11-25 11:49:48 +0530

    export and select all done

--- src/EmpListing.js
+++ src/EmpListing.js
@@ -1,102 +1,232 @@
 import { useEffect, useState } from "react";
+
 import { Link, useNavigate } from "react-router-dom";
 
+import { CSVDownload, CSVLink} from "react-csv";
 const EmpListing = () => {
-    const [empdata, empdatachange] = useState(null);
+
+    const [empdata, empdatachange] = useState([]);
+
+    const[check,setchecked]=useState([]);
+
+    const [checkAll, setCheckAll] = useState(false);
+
     const navigate = useNavigate();
 
-    const LoadDetail = (id) => {
-        navigate("/employee/detail/" + id);
-    }
-    const LoadEdit = (id) => {
-        navigate("/employee/edit/" + id);
-    }
-    const Removefunction = (id) => {
-        if (window.confirm('Do you want to remove?')) {
-            fetch("http://localhost:8000/employee/" + id, {
-                method: "DELETE"
-            }).then((res) => {
-                alert('Removed successfully.')
-                window.location.reload();
-            }).catch((err) => {
-                console.log(err.message)
-            })
-        }
-    }
-
-
-
+  //getdata
 
     useEffect(() => {
-        fetch("http://localhost:8000/employee").then((res) => {
-            return res.json();
-        }).then((resp) => {
-            empdatachange(resp);
-        }).catch((err) => {
-            console.log(err.message);
-        })
-    }, [])
+
+        const getEmpData=async() => {
+
+            const empreq=await fetch("http://localhost:8000/employee");
+
+            const empres=await empreq.json();
+
+            console.log(empres);
+
+            empdatachange(empres);
+
+        }
+
+        getEmpData();
+
+    }
+
+ , []
+
+    );
+
+   
+
+ 
+
+    const LoadDetail = (id) => {
+
+        navigate("/employee/detail/" + id);
+
+    }
+
+    const LoadEdit = (id) => {
+
+        navigate("/employee/edit/" + id);
+
+    }
+
+    const Removefunction = (id) => {
+
+        if (window.confirm('Do you want to remove?')) {
+
+            fetch("http://localhost:8000/employee/" + id, {
+
+                method: "DELETE"
+
+            }).then((res) => {
+
+                alert('Removed successfully.')
+
+                window.location.reload();
+
+            }).catch((err) => {
+
+                console.log(err.message)
+
+            })
+
+        }
+
+    }
+
+ 
+
+   
+
+   
+
     return (
+
         <center>
+
         <div className="container">
-           
+
             <div className="card cardsize">
+
                 <div className="card-title">
+
                     <h2>Employee Listing</h2>
+
                 </div>
+
                 <div className="card-body">
-                    <div className="divbtn">
+
+            <div className="divbtn">
+
                         <Link to="employee/create" className="btn btn-success">Add New (+)</Link>
+
                     </div>
+
+ 
+
                     <div className="divbtn">
-                        <button className="btn btn-primary">Export(↓)</button>
+
+                       <CSVLink data={empdata} filename="Employee Data.xlsx" className="btn btn-primary">Export(↓)</CSVLink>
+
                     </div>
+
+                   
+
+                   
+
                     <div className="divbtn">
-                        <button className="btn btn-warning">Select All(*)</button>
-                    </div>
-                    <div className="divbtn">
+
                         <button className="btn btn-danger">Remove(-)</button>
+
                     </div>
+
+                   
+
                     <table className="table table-bordered table table-hover fixed_header">
+
                         <thead className="bg-dark text-white stickheader">
+
                             <tr>
-                            <td></td>
-                                <td>ID</td>
+
+                                <td><input type="checkbox"
+
+                                onChange={ e=>{
+
+                                    let checked=e.target.checked;
+
+                                    empdatachange(empdata.map(d=>{
+
+                                        d.select=checked;
+
+                                       
+
+                                        return d;
+
+                                       
+
+                                       
+
+                                    }));
+
+                                }}/> ID</td>
+
                                 <td>Name</td>
+
                                 <td>Email</td>
+
                                 <td>Phone</td>
+
                                 <td>Action</td>
+
                             </tr>
+
                         </thead>
+
                         <tbody>
 
-                            {empdata &&
+ 
+
+                            {
+
                                 empdata.map(item => (
-                                    
+
                                     <tr key={item.id}>
-                                        <td><input type="checkbox"/>
-                                    </td>
-                                        <td>{item.id}</td>
+
+                                        <td>
+
+                                            <input
+
+                                             type="checkbox" checked={item.select}/> {item.id}</td>
+
                                         <td>{item.name}</td>
+
                                         <td>{item.email}</td>
+
                                         <td>{item.phone}</td>
+
                                         <td><a onClick={() => { LoadEdit(item.id) }} className="btn btn-success">Edit</a>
+
                                             <a onClick={() => { Removefunction(item.id) }} className="btn btn-danger">Remove</a>
+
                                             <a onClick={() => { LoadDetail(item.id) }} className="btn btn-primary">Details</a>
+
                                         </td>
+
                                     </tr>
+
                                 ))
+
                             }
+
+ 
 
                         </tbody>
 
+ 
+
                     </table>
+
+                   
+
+                   
+
                 </div>
+
             </div>
-          
+
         </div>
+
         </center>
+
     );
+
 }
 
+ 
+
 export default EmpListing;
+
+ 
