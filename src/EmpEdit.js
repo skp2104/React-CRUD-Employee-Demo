e6bc8519d2334154f1d38a705ed9bb7b437dca53
Shravan Kumar Pandit <109153292+skp2104@users.noreply.github.com>
2022-11-30 12:16:54 +0530

    Update EmpEdit.js

--- src/EmpEdit.js
+++ src/EmpEdit.js
@@ -1,122 +1,152 @@
 import { useEffect, useState } from "react";
+
 import { Link, useNavigate, useParams } from "react-router-dom";
 
+//ASHOK AND SHRAVAN
+
 const EmpEdit = () => {
-    const { empid } = useParams();
+  const { empid } = useParams();
 
-    //const [empdata, empdatachange] = useState({});
+  //const [empdata, empdatachange] = useState({});
 
-    useEffect(() => {
-        fetch("http://localhost:8000/employee/" + empid).then((res) => {
-            return res.json();
-        }).then((resp) => {
-            idchange(resp.id);
-            namechange(resp.name);
-            emailchange(resp.email);
-            phonechange(resp.phone);
-            activechange(resp.isactive);
-        }).catch((err) => {
-            console.log(err.message);
-        })
-    }, []);
-
-    const[id,idchange]=useState("");
-    const[name,namechange]=useState("");
-    const[email,emailchange]=useState("");
-    const[phone,phonechange]=useState("");
-    const[active,activechange]=useState(true);
-    const[validation,valchange]=useState(false);
-
-
-    const navigate=useNavigate();
-
-    const handlesubmit=(e)=>{
-      e.preventDefault();
-      const empdata={id,name,email,phone,active};
-
-
-      fetch("http://localhost:8000/employee/"+empid,{
-        method:"PUT",
-        headers:{"content-type":"application/json"},
-        body:JSON.stringify(empdata)
-      }).then((res)=>{
-        alert('Saved successfully.')
-        navigate('/');
-      }).catch((err)=>{
-        console.log(err.message)
+  useEffect(() => {
+    fetch("http://localhost:8000/employee/" + empid)
+      .then((res) => {
+        return res.json();
       })
+      .then((resp) => {
+        idchange(resp.id);
 
-    }
-    return ( 
-        <div>
+        namechange(resp.name);
 
-        <div className="row">
-            <div className="offset-lg-3 col-lg-6">
-                <form className="container" onSubmit={handlesubmit}>
+        emailchange(resp.email);
 
-                    <div className="card" style={{"textAlign":"left"}}>
-                        <div className="card-title">
-                            <h2>Employee Edit</h2>
-                        </div>
-                        <div className="card-body">
+        phonechange(resp.phone);
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  }, []);
 
-                            <div className="row">
+  const [id, idchange] = useState("");
 
-                                <div className="col-lg-12">
-                                    <div className="form-group">
-                                        <label>ID</label>
-                                        <input value={id} disabled="disabled" className="form-control"></input>
-                                    </div>
-                                </div>
+  const [name, namechange] = useState("");
 
-                                <div className="col-lg-12">
-                                    <div className="form-group">
-                                        <label>Name</label>
-                                        <input required value={name} onMouseDown={e=>valchange(true)} onChange={e=>namechange(e.target.value)} className="form-control"></input>
-                                    {name.length==0 && validation && <span className="text-danger">Enter the name</span>}
-                                    </div>
-                                </div>
+  const [email, emailchange] = useState("");
 
-                                <div className="col-lg-12">
-                                    <div className="form-group">
-                                        <label>Email</label>
-                                        <input value={email} onChange={e=>emailchange(e.target.value)} className="form-control"></input>
-                                    </div>
-                                </div>
+  const [phone, phonechange] = useState("");
 
-                                <div className="col-lg-12">
-                                    <div className="form-group">
-                                        <label>Phone</label>
-                                        <input value={phone} onChange={e=>phonechange(e.target.value)} className="form-control"></input>
-                                    </div>
-                                </div>
+  const [validation, valchange] = useState(false);
 
-                                <div className="col-lg-12">
-                                    <div className="form-check">
-                                    <input checked={active} onChange={e=>activechange(e.target.checked)} type="checkbox" className="form-check-input"></input>
-                                        <label  className="form-check-label">Is Active</label>
+  const navigate = useNavigate();
 
-                                    </div>
-                                </div>
-                                <div className="col-lg-12">
-                                    <div className="form-group">
-                                       <button className="btn btn-success" type="submit">Save</button>
-                                       <Link to="/" className="btn btn-danger">Back</Link>
-                                    </div>
-                                </div>
+  const handlesubmit = (e) => {
+    e.preventDefault();
 
-                            </div>
+    const empdata = { id, name, email, phone };
 
-                        </div>
+    fetch("http://localhost:8000/employee/" + empid, {
+      method: "PUT",
 
+      headers: { "content-type": "application/json" },
+
+      body: JSON.stringify(empdata),
+    })
+      .then((res) => {
+        alert("Saved successfully.");
+
+        navigate("/");
+      })
+      .catch((err) => {
+        console.log(err.message);
+      });
+  };
+
+  return (
+    <div>
+      <div className="row">
+        <div className="offset-lg-3 col-lg-6">
+          <form className="container" onSubmit={handlesubmit}>
+            <div className="card" style={{ textAlign: "left" }}>
+              <div className="card-title">
+                <h2>Employee Edit</h2>
+              </div>
+
+              <div className="card-body">
+                <div className="row">
+                  <div className="col-lg-12">
+                    <div className="form-group">
+                      <label>ID</label>
+
+                      <input
+                        value={id}
+                        disabled="disabled"
+                        className="form-control"
+                      ></input>
                     </div>
+                  </div>
 
-                </form>
+                  <div className="col-lg-12">
+                    <div className="form-group">
+                      <label>Name</label>
 
+                      <input
+                        required
+                        value={name}
+                        onMouseDown={(e) => valchange(true)}
+                        onChange={(e) => namechange(e.target.value)}
+                        className="form-control"
+                      ></input>
+
+                      {name.length == 0 && validation && (
+                        <span className="text-danger">Enter the name</span>
+                      )}
+                    </div>
+                  </div>
+
+                  <div className="col-lg-12">
+                    <div className="form-group">
+                      <label>Email</label>
+
+                      <input
+                        value={email}
+                        onChange={(e) => emailchange(e.target.value)}
+                        className="form-control"
+                      ></input>
+                    </div>
+                  </div>
+
+                  <div className="col-lg-12">
+                    <div className="form-group">
+                      <label>Phone</label>
+
+                      <input
+                        value={phone}
+                        onChange={(e) => phonechange(e.target.value)}
+                        className="form-control"
+                      ></input>
+                    </div>
+                  </div>
+
+                  <div className="col-lg-12">
+                    <div className="form-group">
+                      <button className="btn btn-success" type="submit">
+                        Save
+                      </button>
+
+                      <Link to="/" className="btn btn-danger">
+                        Back
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
+          </form>
         </div>
+      </div>
     </div>
-     );
-}
+  );
+};
 
 export default EmpEdit;
